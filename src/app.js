8bb0ce3c577343740acc18d7083296ce5a8405f2
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-13 20:42:09 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = SECRET_PORT;
+
+app.use(express.json);
+
+app.use("/api");
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
