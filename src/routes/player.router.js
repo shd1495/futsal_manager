6af1f8f4c9ca9dd07a1b3e74d74e4721fa5d973d
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-15 17:08:54 +0900

    fix: "팀 편성 API 인증 미들웨어 추가"
    인증 미들웨어를 거치도록 수정

--- src/routes/player.router.js
+++ src/routes/player.router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createLineUp } from '../controllers/player.controller.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 /**
  * 팀 편성 API
  */
-router.post('/lineup/:accountId', createLineUp);
+router.post('/lineup/:accountId', authMiddleware, createLineUp);
 
 export default router;
