d60566459342a8dfa256ee48cd4a3f3c42cf94f7
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-15 16:57:59 +0900

    fix: "에러 처리 미들웨어 import"
    에러 처리 미들웨어 사용 등록

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import errorHandleMiddleware from './middlewares/error.handle.middleware.js';
 import { playerRouter } from './routes/player.router.js';
 
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = SECRET_PORT;
 app.use(express.json);
 
 app.use('/api', playerRouter);
+app.use(errorHandleMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
