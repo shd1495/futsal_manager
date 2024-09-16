2ee7106c6082a8d5e9f0ee66cf639ef936050c15
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-16 23:07:56 +0900

    feat: "선수 판매 API 추가"
    라우터에 선수 판매 API 등록

--- src/routes/player.router.js
+++ src/routes/player.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createLineUp } from '../controllers/player.controller.js';
+import { createLineUp, sellPlayer } from '../controllers/player.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -8,5 +8,10 @@ const router = express.Router();
  * 팀 편성 API
  */
 router.post('/lineup/:accountId', authMiddleware, createLineUp);
+
+/**
+ * 선수 판매 API
+ */
+router.post('/sell/:accountId', authMiddleware, sellPlayer);
 
 export default router;
