7f35820695578c30d6967ee553051c3c5afc06f2
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-16 23:15:27 +0900

    comment: "매치메이킹 API 주석 추가"

--- src/routes/game.router.js
+++ src/routes/game.router.js
@@ -4,6 +4,9 @@ import { matchMaking } from '../controllers/game.controller.js';
 
 const router = express.Router();
 
+/**
+ * 매치메이킹 API
+ */
 router.post('/match/:accountId', authMiddleware, matchMaking);
 
 export default router;
