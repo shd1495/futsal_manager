3546d6ca9001b8f7c17b1673f01a4cbf520489cc
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-18 14:44:47 +0900

    feat: "보유 선수 목록 API 추가"
    보유 선수 목록 조회 함수 getPlayer import, 보유 선수 목록 조회 API 추가

--- src/routes/player.router.js
+++ src/routes/player.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createLineup, sellPlayer } from '../controllers/player.controller.js';
+import { createLineup, sellPlayer, getPlayers } from '../controllers/player.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -13,5 +13,10 @@ router.post('/lineup/:accountId', authMiddleware, createLineup);
  * 선수 판매 API
  */
 router.post('/sell/:accountId', authMiddleware, sellPlayer);
+
+/**
+ * 보유 선수 목록 조회 API
+ */
+router.get('/myPlayers/:accountId', authMiddleware, getPlayers);
 
 export default router;
