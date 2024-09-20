dcce3cebe65d13f04ad6f5ca7f3b5b775dba767c
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-20 18:16:25 +0900

    bugfix: "계정 관련 api 전체 수정"
    회원가입 - 유효성 검사 수정 status코드 수정
    로그인 - 토큰 생성시 id값 변경
    삭제, 계정 조회 - 요청 파라미터 및 checkAccount 파라미터 수정

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,4 +1,5 @@
 import { throwError } from '../utils/error.handle.js';
+import { prisma } from '../utils/prisma/index.js';
 
 class AccountService {
   constructor(prisma) {
@@ -9,6 +10,7 @@ class AccountService {
     const account = await prisma.accounts.findUnique({
       where: { accountId },
     });
+
     if (!account) throw throwError('계정을 찾을 수 없습니다.', 404);
     else if (authAccountId !== undefined && authAccountId !== accountId)
       throw throwError('권한이 없습니다.', 403);
