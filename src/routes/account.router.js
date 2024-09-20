815fd452b4adec14066b503ffeb393dad05d395e
jegal01 <wis05286@gmail.com>
2024-09-20 12:39:15 +0900

    feat"계정삭제,조회 api추가"
    계정삭제,조회 api추가

--- src/routes/account.router.js
+++ src/routes/account.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middlewares/auth.middleware.js';
-import { signAccount, loginAccount } from '../controllers/account.controller.js';
+import { signAccount, loginAccount, deleteAccount, inquireAccount } from '../controllers/account.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,9 @@ const router = express.Router();
 router.post('/sign/:accountId', signAccount);
 //로그인
 router.post('/login/:accountId', loginAccount);
+//계정 삭제
+router.delete(`/delete/:accountId`,deleteAccount);
+//계정조회
+router.get(`/inquire/:accountId`,inquireAccount);
 
 export default router;
