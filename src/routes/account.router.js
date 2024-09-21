2de8d1fdfcf4762eeee0d7871596ec117ad3f2c1
박지영 <124862122+shd1495@users.noreply.github.com>
2024-09-21 17:32:33 +0900

    fix: "회원가입, 계정 삭제, 조회 API 수정"

--- src/routes/account.router.js
+++ src/routes/account.router.js
@@ -10,12 +10,12 @@ import {
 const router = express.Router();
 
 //회원가입
-router.post('/sign', signAccount);
+router.post('/register', signAccount);
 //로그인
 router.post('/login', loginAccount);
 //계정 삭제
-router.delete(`/delete/:accountId`, authMiddleware, deleteAccount);
+router.delete(`/account/:accountId`, authMiddleware, deleteAccount);
 //계정 조회
-router.get(`/inquire/:accountId`, authMiddleware, inquireAccount);
+router.get(`/account/:accountId`, authMiddleware, inquireAccount);
 
 export default router;
